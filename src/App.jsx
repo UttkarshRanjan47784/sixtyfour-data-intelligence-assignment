f9774492dc7ca42217510afc08c1f93a8065af46
UttkarshRanjan47784 <utr.1206@gmail.com>
2024-04-20 02:28:05 +0530

    Removed Useless Stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { ThemeProvider } from "./components/theme-provider";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BlankPage from "./pages/BlankPage";
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/blank" element={<BlankPage />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blank" element={<BlankPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
