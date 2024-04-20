320200bf6445d4a0a81f0a8d13a4be75e6735534
UttkarshRanjan47784 <utr.1206@gmail.com>
2024-04-20 15:26:26 +0530

    Responsive Update 2. Made the user saved libraries section responsive

--- src/components/contentwrapper/UserSavedLibraries.jsx
+++ src/components/contentwrapper/UserSavedLibraries.jsx
@@ -1,5 +1,7 @@
 import { FolderClosed } from "lucide-react";
 import React from "react";
+import Carousel1 from "../usersavedlibs/Carousel1";
+import Carousel3 from "../usersavedlibs/Carousel3";
 
 export default function UserSavedLibraries() {
   const colArr = [
@@ -11,7 +13,24 @@ export default function UserSavedLibraries() {
     `text-yellow-500`,
   ];
 
-  const renderSavedLibs = [...Array(5)].map((item, index) => {
+  const renderSavedLibs5 = [...Array(5)].map((item, index) => {
+    return (
+      <div
+        className="bg-background h-[170px] shadow-md rounded-lg flex justify-center items-center px-5 hover:scale-110 transition-all duration-300"
+        key={`savedLib${index}${Math.random()}`}
+      >
+        <div className="w-full">
+          <FolderClosed className={`${colArr[index % colArr.length]}`} />
+          <h3 className="font-semibold mt-3">My Saved Library {index + 1}</h3>
+          <p className="text-muted-foreground mt-3 text-sm font-semibold">
+            dd-mm-yyyy
+          </p>
+        </div>
+      </div>
+    );
+  });
+
+  const renderSavedLibs3 = [...Array(3)].map((item, index) => {
     return (
       <div
         className="bg-background h-[170px] shadow-md rounded-lg flex justify-center items-center px-5 hover:scale-110 transition-all duration-300"
@@ -29,6 +48,16 @@ export default function UserSavedLibraries() {
   });
 
   return (
-    <div className="px-10 py-3 grid grid-cols-5 gap-7">{renderSavedLibs}</div>
+    <>
+      <div className="hidden px-10 py-3 min-[870px]:grid grid-cols-5 gap-7">
+        {renderSavedLibs5}
+      </div>
+      <div className="hidden px-10 py-3 min-[870px]:hidden sm:flex sm:justify-center sm:items-center">
+        <Carousel3 />
+      </div>
+      <div className="px-10 py-3 sm:hidden flex justify-center items-center">
+        <Carousel1 />
+      </div>
+    </>
   );
 }
